1585e39a7688c5e6645b378131eb8c2eac4c44f5
Olivier Goffart <olivier.goffart@slint.dev>
2026-04-12 14:21:28 +0000

    Add element doc extraction from builtins.slint.
    Parse /// and //! doc comments using the i-slint-compiler parser.
    Handle inheritance, section headers, sub-elements (recursive),
    and special annotations (\default, \footer, \skip_inherited).
    Generate mdx files for all documented builtin elements.

--- docs/slint-doc-generator/mdx.rs
+++ docs/slint-doc-generator/mdx.rs
@@ -11,6 +11,7 @@ pub fn generate() -> Result<(), Box<dyn std::error::Error>> {
     generate_enum_docs()?;
     generate_builtin_struct_docs()?;
     generate_keys_docs()?;
+    crate::element_docs::generate()?;
 
     let root = crate::root_dir();
     let enums = extract_enum_docs();
